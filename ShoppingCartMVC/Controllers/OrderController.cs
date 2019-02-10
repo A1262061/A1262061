9c91f7aca85c3392900d4bf2af20638d17fcfe2e
Deng-You-Jie <dicka1262061@gmail.com>
2019-02-10 20:09:09 +0800

    v1

---
+++ ShoppingCartMVC/Controllers/OrderController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartMVC.Models;
+namespace ShoppingCartMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        ShoppingCartEntities db = new ShoppingCartEntities();
+        // GET: Order
+        public ActionResult CreateOrder()
+        {
+
+            return View();
+        }
+        public ActionResult OrderList()
+        {
+            if (Session["Member"] == null)
+            {
+                return RedirectToAction("SignIn", "Sign");
+            }
+            int UserId = int.Parse(Session["Member"].ToString());
+            var OrderList = db.Orders.Where(m => m.M_num == UserId).ToList();
+            return View("OrderList", "_LayoutMember",OrderList);
+
+        }
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var Order_Item = db.Orders_Items.Where(m => m.O_num == id).ToList();
+
+
+            return View("Details", "_LayoutMember", Order_Item);
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateOrder(string O_Address,string O_Name,string O_Phone)
+        {
+
+            Random random = new Random();
+            var UserId = Session["Member"].ToString();
+            List<Cart> Cart = db.Cart.Where(m => m.M_num == UserId).ToList();
+            var TotalPrice = db.Cart.Where(m => m.M_num == UserId).Select(m => m.P_price).Sum();
+            Orders Create_Orders = new Orders();
+            Create_Orders.O_Date = DateTime.Now;
+            Create_Orders.O_Address = O_Address;
+            Create_Orders.O_num = random.Next(5, 788);
+            Create_Orders.O_Phone = O_Phone;
+            Create_Orders.O_Name = O_Name;
+            Create_Orders.M_num = int.Parse(UserId);
+            Create_Orders.Price = (int)TotalPrice;
+            Create_Orders.Status = "NoReady";
+            db.Orders.Add(Create_Orders);
+
+            int Count = 0;
+            foreach (var Item in Cart)
+            {
+                Orders_Items Orders_Detial = new Orders_Items();
+                Orders_Detial.Amount = (int)Item.Amount;
+                Orders_Detial.OItems_num = Count;
+                Orders_Detial.P_name = Item.P_name;
+                Orders_Detial.O_num = Create_Orders.O_num;
+                Orders_Detial.P_Style = Item.P_style;
+                db.Cart.Remove(Item);
+                db.Orders_Items.Add(Orders_Detial);
+                Count++;
+
+            }
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "Index");
+
+
+        }
+
+    }
+}
---
+++ ShoppingCartMVC/Controllers/SignController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartMVC.Models;
+
+namespace ShoppingCartMVC.Controllers
+{
+    public class SignController : Controller
+    {
+        ShoppingCartEntities db = new ShoppingCartEntities();
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(string Account, string Password)
+        {
+
+            Member member = db.Member.Where(m => m.M_Password == Password && m.M_Account == Account).FirstOrDefault();
+            if (member == null)
+            {
+                ViewBag.Mes = "Error";
+                return View();
+
+            }
+            Session["Member"] = member.M_num;
+
+            return RedirectToAction("Index", "Index");
+        }
+        // GET: Members/Create
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        // POST: Members/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp([Bind(Include = "M_num,M_Name,M_Account,M_Password,M_Tel,M_Gender,M_Address")] Member member)
+        {
+            var Member = db.Member.Where(m => m.M_Account == member.M_Account).FirstOrDefault();
+            if(Member!=null)
+            {
+                ViewBag.Mes = "Repeat";
+                return View();
+            }
+            
+            Random rd = new Random();
+            if (ModelState.IsValid)
+            {
+                member.M_num =rd.Next(20,1245);
+                db.Member.Add(member);
+                db.SaveChanges();
+                return RedirectToAction("SignIn", "Sign");
+            }
+
+            return View();
+
+        }
+
+    }
+
+}
