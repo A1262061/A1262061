2fdc73df855758918dbf5dc7b2ab39b5f012f514
Deng-You-Jie <dicka1262061@gmail.com>
2019-02-10 20:09:09 +0800

    v1

---
+++ ShoppingCartMVC/Controllers/IndexController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartMVC.Models;
+namespace ShoppingCartMVC.Controllers
+{
+    public class IndexController : Controller
+    {
+        ShoppingCartEntities db = new ShoppingCartEntities();
+        // GET: Index
+        public ActionResult Index(string searchString)
+        {
+            var Product = db.Product.OrderBy(m => Guid.NewGuid()).ToList().Take(6);
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                Product = db.Product.Where(s => s.P_Name.Contains(searchString));
+            }
+            if (Session["Member"] != null)
+            {
+                return View("Index", "_LayoutMemberIndex", Product);
+            }
+            else
+            {
+                return View("Index", "_LayoutIndex", Product);
+
+            }
+        }
+        [ChildActionOnly]
+        public ActionResult ShowBanner()
+        {
+            var List = db.Product.OrderBy(m => Guid.NewGuid()).Take(3).ToList();
+            return PartialView("_ShowBanner", List);
+        }
+
+    }
+}
---
+++ ShoppingCartMVC/Controllers/ProductPageController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartMVC.Models;
+
+namespace ShoppingCartMVC.Controllers
+{
+    public class ProductPageController : Controller
+    {
+        ShoppingCartEntities db = new ShoppingCartEntities();
+        // GET: ProductPage
+        public ActionResult ProductPage(int? Id)
+        {
+            var Product = db.Product.Where(m=>m.P_num==Id).ToList();
+            if(Session["Member"]==null)
+            {
+                return View("ProductPage", "_LayoutMember", Product);
+
+               
+            }
+            return View("ProductPage", "_Layout", Product);
+        }
+        [ChildActionOnly]
+        public ActionResult ImgList(int? ProductId)
+        {
+            var ImgList=db.Product.Where(m => m.P_num != ProductId).OrderBy(m => Guid.NewGuid()).Take(4).ToList();
+            return PartialView("_ProductPageImgList", ImgList);
+        }
+        [ChildActionOnly]
+        public ActionResult Style(int? ProductId)
+        {
+            
+            var Style = db.ProductStyle.Where(m => m.P_num == ProductId).ToList();
+            return PartialView("_ProductPageStyle", Style);
+          
+        }
+    }
+}
