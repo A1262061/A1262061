13b9679522897db765868ee783a92e312495e699
Deng-You-Jie <dicka1262061@gmail.com>
2019-02-10 22:38:06 +0800

    v2

---
+++ ShoppingCartMVC/Controllers/JoinController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingCartMVC.Models;
+
+namespace ShoppingCartMVC.Controllers
+{
+    public class JoinController : Controller
+    {
+        ShoppingCartEntities db = new ShoppingCartEntities();
+        // GET: Join
+        public ActionResult Index()
+        {
+         
+
+            var join = (from Orders in db.Orders
+                       join item in db.Orders_Items
+                       on Orders.O_num equals item.O_num
+                       select new Join { O_num=Orders.O_num,Amount=item.Amount,O_Name=Orders.O_Name}).ToList();
+        
+            return View(join);
+        }
+    }
+}
---
+++ ShoppingCartMVC/Models/Join.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCartMVC.Models
+{
+    public class Join
+    {
+        public int Amount { get; set; }
+        public int O_num { get; set; }
+        public string O_Name { get; set; }
+
+
+    }
+}
